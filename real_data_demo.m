a8618072272c24b58246109f70ac6323703cd8a4
HYfromLA <106124968+HYfromLA@users.noreply.github.com>
2025-06-15 15:42:28 -0600

    Add files via upload

--- real_data_demo.m
+++ real_data_demo.m
@@ -48,9 +48,9 @@ LAPDopts.bandwidth = 14; LAPDopts.weight = "two sided";
 [intrinsicDim,epsilon,k_hat,labels,time,misc] = main(X,LAPDopts);
 OA= accuracy(labels, labelsGT);
 
-%[0:7] bandwidth = 14, denoising=default, SKNN=default: 96.13%
-%[0:8] bandwidth = 14, denoising=default, SKNN=default: 94.32%
-%[0:9] bandwidth = 14, denoising=default, SKNN=default: 79.51%
+%[0:7] bandwidth = 14, denoising=default: 96.13%
+%[0:8] bandwidth = 14, denoising=default: 94.32%
+%[0:9] bandwidth = 14, denoising=default: 79.51%
 
 %% MNIST-full. 
 clear
